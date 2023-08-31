acbc69f88c7b6e1453ab50f77396d94cd888a03c
nagesh-jha2001 <111527487+nagesh-jha2001@users.noreply.github.com>
2023-08-31 21:43:52 +0530

    Add files via upload

---
+++ config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI:'mongodb+srv://NageshJha:873083@cluster0.qrxz5ba.mongodb.net/?retryWrites=true&w=majority'
+}
---
+++ models/Product.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const productSchema = mongoose.Schema({
+    writer: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    title: {
+        type: String,
+        maxlength: 50
+    },
+    description: {
+        type: String
+    },
+    price: {
+        type: Number,
+        default: 0
+    },
+    images: {
+        type: Array,
+        default: []
+    },
+    categories: {
+        type: Number,
+        default: 1
+    }
+}, { timestamps: true })
+
+
+productSchema.index({ 
+    title:'text',
+    description: 'text',
+}, {
+    weights: {
+        name: 5,
+        description: 1,
+    }
+})
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = { Product }
